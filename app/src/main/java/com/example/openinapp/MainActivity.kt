8f8d9bac319631a38f680ae660dc4661c6059cc6
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2023-06-13 11:08:56 +0530

    fragment dashboard

--- app/src/main/java/com/example/openinapp/MainActivity.kt
+++ app/src/main/java/com/example/openinapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.openinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.example.openinapp.UI.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.commit {
+            add(R.id.container,MainFragment())
+        }
     }
 }
